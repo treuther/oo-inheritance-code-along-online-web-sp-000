dfb599ebb661b1bc442a08291ff37939302ce5b3
Tracy Reuther <tracyreuther@gmail.com>
2019-07-07 21:13:35 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,4 +1,5 @@
 class Vehicle #this is the parent (SUPER class)
+  
   attr_accessor :wheel_size, :wheel_number
   
   def initialize(wheel_size, wheel_number)
@@ -7,6 +8,11 @@ class Vehicle #this is the parent (SUPER class)
   end
   
   def go
-    "v"
+    "vrrrrrrrooom!"
+  end
+  
+  def fill_up_tank
+    "filling up!"
+  end
     
 end
