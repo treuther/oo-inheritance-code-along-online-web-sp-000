209816e166c846233a78aac0371c95085d95604a
Tracy Reuther <tracyreuther@gmail.com>
2019-07-07 21:13:05 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,12 @@
 class Vehicle #this is the parent (SUPER class)
-
+  attr_accessor :wheel_size, :wheel_number
+  
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+  
+  def go
+    "v"
+    
 end
